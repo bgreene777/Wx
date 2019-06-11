ff634896b706b9287eb76fb31ce2e1084deecd62
Brian Greene <bgreene777@gmail.com>
2019-06-11 11:50:10 -0500

    more command line args and convert to f strings

--- NWCmesonet.py
+++ NWCmesonet.py
@@ -28,10 +28,27 @@ parser.add_argument('-f', required=True, action='store', dest='fig', nargs=1,
     help='Save figure? True or False')
 parser.add_argument('-s', required=True, action='store', dest='file', nargs=1,
     help='Save file? True or False')
+parser.add_argument('-save', required=False, action='store', dest='savedir',
+    nargs=1, help='Enter the save directory for file and figure')
+parser.add_argument('-si', required=True, action='store', dest='SI',
+    nargs=1, help='Use SI units? True or False')
 args = parser.parse_args()
 
-# Location to save output files
-saveDir = '/Users/briangreene/Desktop/'
+# Location to save output files and figures
+if args.fig[0].upper() == 'TRUE':
+    saveFig = True
+else:
+    saveFig = False
+
+if args.file[0].upper() == 'TRUE':
+    saveFile = True
+else:
+    saveFile = False
+
+if saveFig | saveFile:
+    saveDir = args.savedir[0]
+else:
+    saveDir = ''
 
 # Base URL
 base_URL = 'http://www.mesonet.org/data/public/nwc/mts-1m/'
@@ -53,8 +70,7 @@ hr = dt_now.hour
 mi = dt_now.minute
 
 # Construct url for mts file
-mts_URL = '{0:s}{1:d}/{2:02d}/{3:02d}/{4:d}{5:02d}{6:02d}nwcm.mts'.format(
-    base_URL, yr, mo, da, yr, mo, da)
+mts_URL = f'{base_URL}{yr}/{mo:02d}/{da:02d}/{yr:02d}{mo:02d}{da:02d}nwcm.mts'
 
 # Fetch data
 df = http.request('GET', mts_URL)
@@ -86,15 +102,25 @@ if now:
 else:
     inow = -2
 
-# Convert RH to Td
-tair = np.array(tair)
+# Convert RH to Td - already read in as degC and m/s
+tair = np.array(tair) * units.degC
 relh = np.array(relh)
-wspd = np.array(wspd)
-td = np.array(mcalc.dewpoint_rh(tair[:inow+1] * units.degC, 
-	relh[:inow+1] / 100.))
-wspd_kts = wspd * 1.94
-tair_F = tair * 1.8 + 32.
-td_F = td * 1.8 + 32.
+wspd = np.array(wspd) * units.m / units.s
+td = np.array(mcalc.dewpoint_rh(tair[:inow+1], relh[:inow+1] / 100.))
+
+if args.SI[0].upper() == 'TRUE':
+    # already in SI, just grab magnitudes
+    tair = tair.magnitude
+    td = td.magnitude
+    wspd = wspd.magnitude
+    tlab = '$^\circ$F'
+    wslab = 'kts'
+else:
+    tair = tair.to(units.degF).magnitude
+    td = td.to(units.degF).magnitude
+    wspd = wspd.to(units.kts).magnitude
+    tlab = '$^\circ$C'
+    wslab = 'm s$^{-1}$'
 
 # Calculate Wind Chill
 #wchl = mcalc.windchill(tair_F[:inow+1]*units.degF, wspd_kts[:inow+1]*units.kts)
@@ -115,17 +141,16 @@ t_all = mpdates.date2num(dt_all)
 
 # Print current conditions
 print('Current conditions at NWC Mesonet:')
-print('Date: {0:s}'.format(datetime.strftime(dt_latest, '%A, %d %B %Y')))
-print('Time: {0:s}'.format(datetime.strftime(dt_latest, '%H:%M UTC')))
-print('Temperature: {0:3.1f}C'.format(tair[inow]))
-print('Dewpoint: {0:3.1f}C'.format(td[inow]))
-print('Wind Speed: {0:3.1f} m s-1'.format(wspd[inow]))
-print('Wind Direction: {0:.0f} deg'.format(wdir[inow]))
+print(f'Date: {datetime.strftime(dt_latest, "%A, %d %B %Y")}')
+print(f'Time: {datetime.strftime(dt_latest, "%H:%M UTC")}')
+print(f'Temperature: {tair[inow]:.1f} degC')
+print(f'Dewpoint: {td[inow]:.1f} degC')
+print(f'Wind Speed: {wspd[inow]:.1f} m/s')
+print(f'Wind Direction: {wdir[inow]:.0f} deg')
 
 # Plot time series of T, Td, p, wspd, wdir
 fig1, axarr = plt.subplots(4, sharex=True, figsize=(10,10))
-figtitle = 'NWC Mesonet Meteogram for {0:s}'.format(datetime.strftime(dt_now,
-	'%d %B %Y'))
+figtitle = f'NWC Mesonet Meteogram for {datetime.strftime(dt_now, "%d %B %Y")}'
 plt.suptitle(figtitle, fontsize=20)
 
 # T & Td
@@ -134,14 +159,14 @@ axarr[0].plot(t_all[:inow+1], tair_F[:inow+1], 'r')
 axarr[0].plot(t_all[:inow+1], td_F, 'g')
 axarr[0].set_title('Temperature and Dewpoint')
 axarr[0].tick_params(labeltop=False, right = True, labelright=True)
-axarr[0].set_ylabel('Temperature ($^\circ$F)')
+axarr[0].set_ylabel(f'Temperature [{tlab}]')
 axarr[0].grid(axis='y')
 
 # p
 axarr[1].plot(t_all[:inow+1], pres[:inow+1], 'k')
 axarr[1].set_title('Pressure')
 axarr[1].tick_params(labeltop=False, right = True, labelright=True)
-axarr[1].set_ylabel('Pressure (hPa)')
+axarr[1].set_ylabel('Pressure[hPa]')
 axarr[1].grid(axis='y')
 
 # wind speed and direction
@@ -149,32 +174,28 @@ axarr_2 = axarr[2].twinx()
 axarr[2].plot(t_all[:inow+1], wspd_kts[:inow+1], 'b')
 axarr_2.plot(t_all[:inow+1], wdir[:inow+1], 'r*', markersize=1)
 axarr[2].set_title('Wind Speed and Direction')
-axarr[2].set_ylabel('Wind Speed (kts)', color='b')
-axarr_2.set_ylabel('Wind Direction ($^\circ$)', color='r')
+axarr[2].set_ylabel(f'Wind Speed [{wslab}]', color='b')
+axarr_2.set_ylabel('Wind Direction [$^\circ$]', color='r')
 axarr[2].grid(axis='y')
 
 # solar radiation
 axarr[3].plot(t_all[:inow+1], srad[:inow+1], 'orange')
 axarr[3].set_title('Solar Radiation')
-axarr[3].set_ylabel('Solar Radiation (W m$^{-2}$)')
+axarr[3].set_ylabel('Solar Radiation [W m$^{-2}$]')
 axarr[3].xaxis.set_major_locator(mpdates.MinuteLocator(interval=60))
 axarr[3].xaxis.set_major_formatter(mpdates.DateFormatter('%H'))
 axarr[3].set_xlabel('Time UTC')
 axarr[3].grid(axis='y')
 
-# Show Plot
-plt.show()
+fig1.tight_layout()
 
 # Save CSV
-s = raw_input('>>Save csv? y/n ')
-while s != 'y' and s != 'n':
-    s = raw_input('>>Save csv? y/n ')
-if s == 'y':
-    saveFileName = '%s%s.%s.1min.csv' % (saveDir, datetime.strftime(dt_latest, 
-    	'%Y%m%d'), 'NWCM')
+if saveFile:
+    saveFileName = f'{saveDir}{datetime.strftime(dt_latest, 
+        "%Y%m%d")}.NWCM.1min.csv'
     headers = ('time', 'relh', 'tair', 'wspd', 'wdir', 'wmax', 'rain', 'pres', 
     	'srad', 'ta9m', 'ws2m', 'skin')
-    fw = open(saveFileName,'wb')
+    fw = open(saveFileName, 'wb')
     writer = csv.writer(fw, delimiter=',')
     writer.writerow(headers)
     for i in range(1440):
@@ -182,6 +203,14 @@ if s == 'y':
         	rain[i], pres[i], srad[i], ta9m[i], ws2m[i], skin[i]) )
 
     fw.close()
-    print('Finished saving {}'.format(saveFileName.split('/')[-1]))
+    print(f'Finished saving {saveFileName.split("/")[-1]}')
+
+# Save plot if saveFig = True, else just show
+if saveFig:
+    fig_name = f'{saveDir}NWC_Meteogram_{dt_base.strftime('%Y%m%d')}.pdf'
+    fig1.savefig(fig_name, format='pdf', dpi=150)
+    print(f'Finished saving {fig_name}')
+else:
+    plt.show()
 
 plt.close('all')
