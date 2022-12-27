53a79fa17e2361fbc7d1bbb50c9456fce1f8a8ab
bgreene777 <bgreene777@gmail.com>
2022-12-27 12:59:24 -0600

    updates to plotting appearance; plot full year 2022 and uploaded to webspace

--- NWCmesonet.py
+++ NWCmesonet.py
@@ -21,6 +21,16 @@ from datetime import datetime, timedelta
 rc('font',weight='normal',size=20,family='serif',serif='Times New Roman')
 rc('text',usetex='True')
 # --------------------------------
+def get_bounds(value_lo, value_hi, multiple):
+    """
+    Calculate upper and lower bound for plotting by rounding up/down 
+    to nearest multiple
+    Return list of 2 values [bound_lo, bound_hi]
+    """
+    bound_lo = multiple * np.floor(value_lo/multiple)
+    bound_hi = multiple * np.ceil(value_hi/multiple)
+    return [bound_lo, bound_hi]
+# --------------------------------
 def plot_NWC(date, savedir=None):
     """
     Grab data from NWC mesonet tower and plot
@@ -34,7 +44,7 @@ def plot_NWC(date, savedir=None):
     mo = date.month
     da = date.day
     # Construct url for mts file
-    mts_URL = f"{base_URL}{yr}/{mo:02d}/{da:02d}/{yr:02d}{mo:02d}{da:02d}nwcm.mts"
+    mts_URL = f"{base_URL}{yr:04d}/{mo:02d}/{da:02d}/{yr:04d}{mo:02d}{da:02d}nwcm.mts"
     # Fetch data
     r = requests.get(mts_URL)
     # parse data
@@ -56,8 +66,8 @@ def plot_NWC(date, savedir=None):
         val[val < -100.] = np.nan
     # convert dictionary to xarray Dataset
     # define time coordinate
-    times = pd.date_range(start=f"{yr}{mo}{da} 12:00AM", 
-                          end=f"{yr}{mo}{da} 11:59PM", freq="min")
+    times = pd.date_range(start=f"{yr:04d}{mo:02d}{da:02d} 12:00AM", 
+                          end=f"{yr:04d}{mo:02d}{da:02d} 11:59PM", freq="min")
     # define dataset
     df = xr.Dataset(data_vars=None, coords=dict(time=times))
     # loop and assign data
@@ -78,7 +88,7 @@ def plot_NWC(date, savedir=None):
     df["SKIN"].attrs["units"] = "$^\\circ$C"
     # full name
     df["RELH"].attrs["name_long"] = "Relative Humidity"
-    df["TAIR"].attrs["name_long"] = "Air Temperature"
+    df["TAIR"].attrs["name_long"] = "1.5m Air Temperature"
     df["WSPD"].attrs["name_long"] = "Wind Speed"
     df["WDIR"].attrs["name_long"] = "Wind Direction"
     df["WMAX"].attrs["name_long"] = "Wind Gust"
@@ -94,7 +104,7 @@ def plot_NWC(date, savedir=None):
     Td = (243.5*np.log(e/6.112)) / (17.67 - np.log(e/6.112))
     df["TDEW"] = xr.DataArray(data=Td, dims="time", coords=dict(time=times),
                               attrs={"units": "$^\\circ$C",
-                                     "name_long": "Dewpoint Temperature"})
+                                     "name_long": "1.5m Dewpoint Temperature"})
     # convert TAIR, TA9M, and TDEW to degF
     df["TAIR_F"] = 1.8 * df.TAIR + 32.
     df["TA9M_F"] = 1.8 * df.TA9M + 32.
@@ -103,58 +113,136 @@ def plot_NWC(date, savedir=None):
     df["WSPD_mph"] = 2.24 * df.WSPD
     df["WMAX_mph"] = 2.24 * df.WMAX
     df["WS2M_mph"] = 2.24 * df.WS2M
+    # # calculate wind chill (1.5m temp, 10m wspd)
+    # df["CHIL_F"] = 35.74 + (0.6215*df.TAIR_F) - (35.75*(df.WSPD_mph**0.16)) +\
+    #                (0.4275*df.TAIR_F*(df.WSPD_mph**0.16))
+    # # calculate heat index (1.5m temp and RH)
+    # T = df.TAIR_F
+    # RH = df.RELH/100.
+    # HI = -42.379 + 2.04901523*T + 10.14333127*RH - .22475541*T*RH -\
+    #      .00683783*T*T - .05481717*RH*RH + .00122874*T*T*RH +\
+    #      .00085282*T*RH*RH - .00000199*T*T*RH*RH
+    # # HI adjustments
+    # if ((RH.values < 0.13) & (T.values > 80.) & (T.values < 112.)):
+    #     HI -=  ((13.-RH)/4.)*np.sqrt((17.-abs(T-95.))/17.)
+    # elif ((RH.values > 0.85) & (T.values > 80.) & (T.values < 87.)):
+    #     HI += ((RH-85.)/10.) * ((87.-T)/5.)
+    # assign to df
+    # df["HEAT_F"] = HI
     # update metadata
     df["TAIR_F"].attrs["units"] = "$^\\circ$F"
     df["TA9M_F"].attrs["units"] = "$^\\circ$F"
     df["TDEW_F"].attrs["units"] = "$^\\circ$F"
+    # df["CHIL_F"].attrs["units"] = "$^\\circ$F"
+    # df["HEAT_F"].attrs["units"] = "$^\\circ$F"
     df["WSPD_mph"].attrs["units"] = "mph"
     df["WMAX_mph"].attrs["units"] = "mph"
     df["WS2M_mph"].attrs["units"] = "mph"
+    # define colors for selected parameters
+    df["TAIR"].attrs["color"] = (204./255, 102./255, 102./255)
+    df["TA9M"].attrs["color"] = (133./255, 22./255, 23./255)
+    df["TDEW"].attrs["color"] = (84./255, 83./255, 179./255)
+    # df["CHIL"].attrs["color"] = (0, 0, 0)
+    # df["HEAT"].attrs["color"] = (0, 0, 0)
+    df["PRES"].attrs["color"] = (0, 0, 0)
+    df["WSPD"].attrs["color"] = (42./255, 37./255, 113./255)
+    df["WDIR"].attrs["color"] = (195./255, 194./255, 122./255)  #(213./255, 94./255, 0)
+    df["SRAD"].attrs["color"] = (255./255, 154./255, 52./255)
+    df["SRAD"].attrs["color2"] = (245./255, 170./255, 95./255)
 
     # begin plotting
-    fig, ax = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(14.8, 15))
+    fig, ax = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(14.8, 12),
+                           constrained_layout=True)
     # title figure
-    figtitle = f"NWC Mesonet {datetime.strftime(date, '%d %B %Y')}"
+    figtitle = f"NWC Mesonet {date.strftime('%d %B %Y')}"
     fig.suptitle(figtitle)
     # T, Td
-    ax[0].plot(df.time, df.TAIR_F, c=(213./255, 94./255, 0), lw=2)
-    ax[0].plot(df.time, df.TDEW_F, c=(0, 158./255, 115./255), lw=2)
-    ax[0].set_title(f"{df.TAIR.name_long} and {df.TDEW.name_long}")
+    ax[0].plot(df.time, df.TAIR_F, c=df.TAIR.color, lw=2, label=df.TAIR.name_long)
+    ax[0].plot(df.time, df.TDEW_F, c=df.TDEW.color, lw=2, label=df.TDEW.name_long)
+    ax[0].plot(df.time, df.TA9M_F, c=df.TA9M.color, lw=2, label=df.TA9M.name_long)
     ax[0].tick_params(labeltop=False, right=True, labelright=True)
     ax[0].set_ylabel(f"Temperature [{df.TAIR_F.units}]")
+    # y-axis limits
+    # lowest value
+    Tlo = np.min([np.nanmin(df.TAIR_F), np.nanmin(df.TDEW_F), 
+                  np.nanmin(df.TA9M_F)])
+    Thi = np.max([np.nanmax(df.TAIR_F), np.nanmax(df.TDEW_F), 
+                  np.nanmax(df.TA9M_F)])
+    Tlim = get_bounds(Tlo, Thi, 5)
+    ax[0].set_ylim(Tlim)
+    # check how wide range of Tlim is
+    if np.diff(Tlim) > 40.:
+        Tmul = 10.
+    else:
+        Tmul = 5.
+    ax[0].yaxis.set_major_locator(MultipleLocator(Tmul))
     ax[0].grid(axis="y")
-    ax[0].yaxis.set_major_locator(MultipleLocator(5))
+    ax[0].legend(frameon=False, labelspacing=0.10, ncol=3, columnspacing=1,
+                 handletextpad=0.4, handlelength=1, fontsize=14,
+                 loc="lower center", bbox_to_anchor=(0.5, 0.95))
+
     # pressure
     ax[1].plot(df.time, df.PRES, c="k", lw=2)
-    ax[1].set_title(df.PRES.name_long)
     ax[1].tick_params(labeltop=False, right=True, labelright=True)
     ax[1].set_ylabel("Pressure [hPa]")
     ax[1].grid(axis="y")
-    ax[1].yaxis.set_major_locator(MultipleLocator(4))
+    # y-axis limits
+    plim = get_bounds(np.nanmin(df.PRES), np.nanmax(df.PRES), 2)
+    ax[1].set_ylim(plim)
+    # check how wide plim is
+    if np.diff(plim) > 10:
+        pmul = 4
+    else:
+        pmul = 2
+    ax[1].yaxis.set_major_locator(MultipleLocator(pmul))
+
     # wind speed and direction
-    ax[2].plot(df.time, df.WSPD_mph, c=(0, 114./255, 178./255), lw=2)
+    ax[2].plot(df.time, df.WSPD_mph, c=df.WSPD.color, lw=2)
     ax2_2 = ax[2].twinx()
-    ax2_2.plot(df.time, df.WDIR, c=(213./255, 94./255, 0), 
-               ls="", marker="o", markersize=1)
-    ax[2].set_title(f"{df.WSPD.name_long} and {df.WDIR.name_long}")
-    ax[2].set_ylabel(f"{df.WSPD.name_long} [mph]", c=(0, 114./255, 178./255))
-    ax2_2.set_ylabel(f"{df.WDIR.name_long}", c=(213./255, 94./255, 0))
+    ax2_2.plot(df.time, df.WDIR, c=df.WDIR.color, 
+               ls="", marker="o", markersize=2)
+    ax[2].set_ylabel(f"{df.WSPD.name_long} [mph]", c=df.WSPD.color)
+    ax2_2.set_ylabel(f"{df.WDIR.name_long}", c=df.WDIR.color)
     ax2_2.set_yticks(range(0, 405, 45))
+    ax2_2.set_ylim(0, 360)
     ax2_2.set_yticklabels(["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"])
     ax[2].grid(axis="y")
-    ax[2].yaxis.set_major_locator(MultipleLocator(5))
+    # y-axis limits
+    wslim = get_bounds(0, np.nanmax(df.WSPD_mph), 5)
+    ax[2].set_ylim(wslim)
+    # check how wide wslim is
+    if np.diff(wslim) > 35:
+        wsmul = 10
+    else:
+        wsmul = 5
+    ax[2].yaxis.set_major_locator(MultipleLocator(wsmul))
+
     # solar radiation
-    ax[3].plot(df.time, df.SRAD, c=(230./255, 159./255, 0), lw=2)
-    ax[3].set_title(df.SRAD.name_long)
+    ax[3].plot(df.time, df.SRAD, c=df.SRAD.color, lw=2, zorder=1001)
+    ax[3].fill_between(df.time, df.SRAD, color=df.SRAD.color2, zorder=1000)
     ax[3].set_ylabel(f"{df.SRAD.name_long} [{df.SRAD.units}]")
     ax[3].tick_params(labeltop=False, right=True, labelright=True)
     ax[3].grid(axis="y")
+    # y-axis limits
+    slim = get_bounds(0, np.nanmax(df.SRAD), 100)
+    ax[3].set_ylim(slim)
+    # check how wide slim is
+    if np.diff(slim) > 1200:
+        smul = 400
+    else:
+        smul = 200
+    ax[3].yaxis.set_major_locator(MultipleLocator(smul))
+
     # x-axis format
     ax[3].set_xlim([df.time[0].values, df.time[0].values+np.timedelta64(1, "D")])
     ax[3].xaxis.set_major_locator(HourLocator(byhour=range(0, 24, 1)))
     ax[3].xaxis.set_major_formatter(DateFormatter("%H"))
     ax[3].set_xlabel("Time UTC")
-    fig.tight_layout()
+    
+    # copyright statement bottom of figure
+    cw = "Copyright 1994-2022 Board of Regents of the University of Oklahoma. All Rights Reserved."
+    ax[3].text(0.5, -0.35, cw, ha="center", va="center", fontsize=14,
+               transform=ax[3].transAxes)
 
     # Save plot if saveFig = True, else just show
     if savedir is not None:
@@ -168,6 +256,8 @@ def plot_NWC(date, savedir=None):
 # Run script if desired
 # --------------------------------
 if __name__ == "__main__":
-    day = datetime(2022, 12, 25)
-    dirsave = "/Users/briangreene/Documents/WxUAS/outputFigures/NWCmesonet/"
-    plot_NWC(day, dirsave)
+    # day = datetime(2022, 12, 22)
+    days = pd.date_range(start="20220101", end="20221226", freq="D")
+    dirsave = "/Users/briangreene/Documents/WxUAS/outputFigures/NWCmesonet/2022/"
+    for day in days:
+        plot_NWC(day, dirsave)
